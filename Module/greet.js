13d126417188440222eeceb9d7fe4d2b0c4e663e
Shashikant Pandey <pandeyshashikant1434@gmail.com>
2026-09-23 21:02:09 +0530

    Module

---
+++ Module/greet.js
@@ -0,0 +1,4 @@
+function greet(name) {
+  console.log("Welcome ", name);
+}
+module.exports = greet;
---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Shashikant Pandey");
+let money = "dont have";
+
+if (money == "have") {
+  console.log("you can buy stuff");
+} else {
+  console.log("get money first");
+}
+console.log(__filename);
+console.log(__dirname);
